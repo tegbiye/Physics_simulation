54e54a6b45f5f7bde9e1e75b67a6fd486fd3e1f4
Tegbabu <se.tegbabu.solomon@gmail.com>
2021-06-18 16:47:45 +0300

    Update pygame_physics.py

--- pygame_physics.py
+++ pygame_physics.py
@@ -1,27 +1,6 @@
 
 import pygame, sys
 
-pygame.init()
-
-#Colors
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-
-# Width and Height
-WIDTH, HEIGHT = 640, 480
-
-
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-
-pygame.display.set_caption('Bouncing Ball SImulation')
-
-# main loop
-
-
-bouncing_ball(BLACK, WHITE, WIDTH, HEIGHT, screen)
-
-
-
 #function which does the logic for the simple collission
 # using pygame module
 def bouncing_ball(col1,col2, wid, height, screen):
@@ -43,4 +22,25 @@ def bouncing_ball(col1,col2, wid, height, screen):
       vy = -vy
     pygame.draw.circle(screen, col2, (ballx, bally), 10)
     pygame.display.update()
+
+
+# main loop
+pygame.init()
+
+#Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+# Width and Height
+WIDTH, HEIGHT = 640, 480
+
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+pygame.display.set_caption('Bouncing Ball SImulation')
+
+bouncing_ball(BLACK, WHITE, WIDTH, HEIGHT, screen)
+
+
+
 
