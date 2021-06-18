aef9c32f01f9a8c0046089ba9ea87503d596b158
Tegbabu <se.tegbabu.solomon@gmail.com>
2021-06-18 17:01:40 +0300

    Update pygame_physicsCopy.py

--- pygame_physicsCopy.py
+++ pygame_physicsCopy.py
@@ -1,2 +1,19 @@
-
+import pygame, sys
 from bouncing_physics import Bouncing
+
+# main loop
+pygame.init()
+
+#Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+# Width and Height
+WIDTH, HEIGHT = 640, 480
+
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+pygame.display.set_caption('Bouncing Ball SImulation')
+
+Bouncing.bouncing_ball(BLACK, WHITE, WIDTH, HEIGHT, screen)
